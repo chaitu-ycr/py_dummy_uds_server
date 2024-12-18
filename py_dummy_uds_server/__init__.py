2d77a3acd70d3d25e9fb996cc367bf3956b9ec8f
chaitu-ycr <67551335+chaitu-ycr@users.noreply.github.com>
2024-12-18 13:31:29 +0530

    updated package class name

--- py_dummy_uds_server/__init__.py
+++ py_dummy_uds_server/__init__.py
@@ -1,7 +1,10 @@
-class MyClass:
+from time import sleep as wait
+
+
+class UdsServer:
     def __init__(self) -> None:
         print("message from my class init.")
 
-    def say_hello(self):
-        print("hello!")
-        return True
+    def process_dignostic_request(self, request: bytes) -> bytes:
+        print("message from my class process_dignostic_request.")
+        return b"response"
--- tests/test_my_package.py
+++ tests/test_my_package.py
@@ -1,6 +1,6 @@
-from py_dummy_uds_server import MyClass
+from py_dummy_uds_server import UdsServer
 
 
 def test_importing():
-    cls_instance = MyClass()
-    assert cls_instance.say_hello()
+    cls_instance = UdsServer()
+    assert cls_instance.process_dignostic_request(b'1001')
